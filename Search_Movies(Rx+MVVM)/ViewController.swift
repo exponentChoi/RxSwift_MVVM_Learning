b5576b5cc1b392ec90c964b9ecef64f2387fdd37
choijisu <jisu@vinnsmail.com>
2021-04-04 17:42:24 +0900

    create project

---
+++ Search_Movies(Rx+MVVM)/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Search_Movies(Rx+MVVM)
+//
+//  Created by 최지수 on 2021/04/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Search_Movies(Rx+MVVM)Tests/Search_Movies_Rx_MVVM_Tests.swift
@@ -0,0 +1,33 @@
+//
+//  Search_Movies_Rx_MVVM_Tests.swift
+//  Search_Movies(Rx+MVVM)Tests
+//
+//  Created by 최지수 on 2021/04/04.
+//
+
+import XCTest
+@testable import Search_Movies_Rx_MVVM_
+
+class Search_Movies_Rx_MVVM_Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
